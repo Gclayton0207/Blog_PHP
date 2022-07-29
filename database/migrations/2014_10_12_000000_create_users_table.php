61db31e4adbfb5de7908f94dbdf18487b5db1d55
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-07-29 15:52:05 -0300

    feat:inserindo campo pra foto

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('lastName');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('thumb');
             $table->timestamps();
         });
     }
