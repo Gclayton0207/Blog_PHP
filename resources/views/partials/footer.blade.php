5216028dd26d49d62bda9ca717bb368bb387e8b4
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-07-30 18:58:04 -0300

    fix: corrigindo footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,13 +3,13 @@
   <hr class="m-0" />
 
   <div class="text-center py-4 align-items-center">
-   
+
   </div>
 
 </div>
 
   <!-- Copyright -->
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2020 Copyright:
-    <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    © 2022 Copyright:
+    <a class="text-dark" href="https://github.com/Gclayton0207" target="_blank"></a>Desenvolvido por Giovanni Clayton</a>
   </div>
