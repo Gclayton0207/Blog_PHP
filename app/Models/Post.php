dc3998f20a63ce8113f7ffa0d22f9d3575ee1e7d
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-07-30 17:57:05 -0300

    feat: criando relacionamentos dos models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public static function search($search)
+    {
+        return self::where('title', "like", "%{$search}%")
+        ->orWhere('content', "like", "%{$search}%")
+        ->with(['user','comments'])
+        ->paginate(3);
+    }
 }
+
+
