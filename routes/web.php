36d52b5bc26f02627af50435880e572b3f5362f5
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-07-30 18:56:06 -0300

    feat: criando pagina e organizando posts

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\PostsController;
 
 
 
 Route::get('/', [HomeController::class,'index'])->name('home');
+Route::get('/posts', [PostsController::class,'index'])->name('posts');
+Route::get('/post/{post:slug}', [PostController::class,'show'])->name('post');
