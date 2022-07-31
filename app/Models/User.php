91f4dd606b95e7e8a91684133acf21d9363c64b8
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-07-30 22:43:14 -0300

    feat: mostrando nome dos usuarios nos posts

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,8 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class);
     }
+    public function getFullnameAttribute()
+    {
+        return $this->firstName . ' '.$this->lastName;
+    }
 }
