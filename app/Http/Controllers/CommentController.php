d1405ad8e911165fedf3072903b19aa67105b349
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-07-31 01:00:01 -0300

    feat: Inserindo sistema de comentarios

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    public function store(Request $request, Post $post)
+    {
+        $validated = $request->validate([
+            'comment' => 'required'
+        ]);
+
+        $created = Comment::create([
+            'comment' => $validated['comment'],
+            'user_id' => auth()->user()->id,
+            'post_id' => $post->id
+        ]);
+
+        if ($created) {
+            return back();
+        }
+
+        return back()->with('error_create_comment', 'Ocorreu um erro ao cadastrar o comentário, tente novamente em alguns segundos');
+    }
+
+    public function destroy(Comment $comment)
+    {
+        // $comment = Comment::find($id);
+
+        $comment->delete();
+
+        return back();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\PostsController;
-use App\Http\Controllers\LoginController;
-
+use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [HomeController::class,'index'])->name('home');
@@ -15,3 +15,5 @@ Route::get('/login', [LoginController::class,'index'])->name('login');
 Route::post('/login', [LoginController::class,'store'])->name('login');
 Route::get('/logout', [LoginController::class,'destroy'])->name('logout');
 Route::get('/posts', [PostsController::class,'index'])->name('posts');
+Route::post('/comment/{post}', [CommentController::class,'store'])->name('comment');
+Route::get('/comment/{comment}', [CommentController::class,'destroy'])->name('comment.destroy');
